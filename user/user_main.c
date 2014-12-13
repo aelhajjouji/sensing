41591c37527332d71ca9ba5c6c3d83a164aa50c4
alm <email@email.fr>
2014-12-13 18:08:20 +0100

    init ok

--- user/user_main.c
+++ user/user_main.c
@@ -6,96 +6,68 @@
 #include "user_interface.h"
 #include "espconn.h"
 #include "mem.h"
-#include "driver/uart.h"
-#include "driver/i2c_master.h"
-#include "driver/adc.h"
+#define user_procTaskPrio 0
+#define user_procTaskQueueLen 1
 
-//#define user_procTaskPrio 0
-//#define user_procTaskQueueLen 1
-
-os_event_t * user_procTaskConnect;
-os_event_t * user_procTaskSerial;
-
+os_event_t user_procTaskQueue[user_procTaskQueueLen];
 //struct espconn* pCon;
-
-//static volatile os_timer_t polling_timer;
+static volatile os_timer_t polling_timer;
 static void loop();
-static void readserial();
 bool connected;
 char temp[64];
-uint16 adc;
-
 
 void polling_timer_elapsed(void *arg) {
 	return;
 }
 
-static void ICACHE_FLASH_ATTR tcpclient_discon_cb(void *arg)
+static void ICACHE_FLASH_ATTR at_tcpclient_discon_cb(void *arg)
 {
-
-	 struct espconn *pespconn = (struct espconn *)arg;
-	os_free(pespconn->proto.tcp);
-	os_free(pespconn);
+	struct espconn *pespconn = (struct espconn *) arg;
 	os_printf("disconnected\r\n");
-	system_deep_sleep(1000*1000*60);
-	//espconn_connect(pespconn);
+	espconn_connect(pespconn);
 
 }
-
-void ICACHE_FLASH_ATTR tcpclient_recv(void *arg, char *pdata, unsigned short len)
+void ICACHE_FLASH_ATTR at_tcpclient_recv(void *arg, char *pdata, unsigned short len)
 {
 	struct espconn *pespconn = (struct espconn *) arg;
 
   	os_printf(pdata);
   	os_printf("\r\n");
-
+	system_deep_sleep(1000*1000*10);
   	//os_sprintf(temp,"?o=%s&d=30\r\n",pdata);
 	//espconn_sent(pespconn, temp, 64);
 }
-
-static void ICACHE_FLASH_ATTR tcpclient_sent_cb(void *arg)
+static void ICACHE_FLASH_ATTR at_tcpclient_sent_cb(void *arg)
 {
-	struct espconn *pespconn = (struct espconn *) arg;
-	os_printf("\r\nSEND OK\r\n");
-	espconn_disconnect(pespconn);
-
+os_printf("\r\nSEND OK\r\n");
 }
 
-
-static void ICACHE_FLASH_ATTR tcpclient_connect_cb(void *arg) {
+static void ICACHE_FLASH_ATTR at_tcpclient_connect_cb(void *arg) {
 	struct espconn *pespconn = (struct espconn *) arg;
 
 	os_printf("tcp client connect\r\n");
 	os_printf("pespconn %p\r\n", pespconn);
-	espconn_regist_disconcb(pespconn, tcpclient_discon_cb);
-	//espconn_regist_recvcb(pespconn, tcpclient_recv); ////////
-	espconn_regist_sentcb(pespconn, tcpclient_sent_cb); ///////
+	espconn_regist_disconcb(pespconn, at_tcpclient_discon_cb);
+	espconn_regist_recvcb(pespconn, at_tcpclient_recv); ////////
+	espconn_regist_sentcb(pespconn, at_tcpclient_sent_cb); ///////
 
-
-
-	adc=system_adc_read();
-	os_sprintf(temp,"GET /new_value.php?o=rabat&d=%d\r\n",adc & 0x3FF);
+	os_sprintf(temp,"GET /new_value.php?o=rabat&d=%d\r\n",system_adc_read());
 	espconn_sent(pespconn, temp, 40);
 
 }
 
-static void ICACHE_FLASH_ATTR tcpclient_recon_cb(void *arg, sint8 errType) {
+static void ICACHE_FLASH_ATTR at_tcpclient_recon_cb(void *arg, sint8 errType) {
 	struct espconn *pespconn = (struct espconn *) arg;
 
 	os_printf("at_tcpclient_recon_cb %p\r\n", arg);
 }
 
-
-
-
 //Main code function
-static void ICACHE_FLASH_ATTR readSensor() {
+static void ICACHE_FLASH_ATTR loop() {
 static struct espconn *pCon;
 //pTcpServer->proto.tcp->local_port=8888;
 
-	while (wifi_station_get_connect_status()!= STATION_GOT_IP )
-		os_delay_us(250000);
-
+	if (wifi_station_get_connect_status()== STATION_GOT_IP&& connected==FALSE) {
 		uint32_t ip = 0;
 		os_printf("connected\r\n");
 		connected = TRUE;
@@ -108,10 +80,13 @@ static struct espconn *pCon;
 		pCon->proto.tcp->remote_port = 80;
 
 		os_memcpy(pCon->proto.tcp->remote_ip, &ip, 4);
-		espconn_regist_connectcb(pCon, tcpclient_connect_cb);
-		espconn_regist_reconcb(pCon, tcpclient_recon_cb);
-		//espconn_regist_time(pCon,500,0);
+		espconn_regist_connectcb(pCon, at_tcpclient_connect_cb);
+		espconn_regist_reconcb(pCon, at_tcpclient_recon_cb);
+		espconn_regist_time(pCon,500,0);
 		espconn_connect(pCon);
+	}
+	else
+		system_os_post(user_procTaskPrio, 0, 0);
 
 	return;
 }
@@ -123,26 +98,26 @@ void user_init(void) {
 	struct station_config stationConf;
 //Set station mode
 	wifi_set_opmode( STATION_MODE);
-	//wifi_set_phy_mode(PHY_MODE_11N);
 //Set ap settings
 	os_memcpy(&stationConf.ssid, ssid, 32);
 	os_memcpy(&stationConf.password, password, 32);
 	wifi_station_set_config(&stationConf);
 
 //Disarm timer
-//	os_timer_disarm(&polling_timer);
+	os_timer_disarm(&polling_timer);
 
 //Setup timer
-//	os_timer_setfn(&polling_timer, (os_timer_func_t *) polling_timer_elapsed,NULL);
+	os_timer_setfn(&polling_timer, (os_timer_func_t *) polling_timer_elapsed,
+	NULL);
 
 //Set up the timer, (timer, milliseconds, 1=cycle 0=once)
-//	os_timer_arm(&polling_timer, 1000, 1);
+	os_timer_arm(&polling_timer, 1000, 1);
 
 //Start os task
-//	system_os_task(loop, 0, user_procTaskConnect,1);
-//	system_os_task(readserial,1,user_procTaskSerial,1);
-//	system_os_post(0, 0, 0);
-readSensor();
+	system_os_task(loop, user_procTaskPrio, user_procTaskQueue,
+	user_procTaskQueueLen);
+	system_os_post(user_procTaskPrio, 0, 0);
+
 //system_deep_sleep(10*1000*1000);
 
 }
